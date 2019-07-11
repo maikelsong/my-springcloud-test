c702855199c7f2edaca4f1674f96f4ffbc17ff95
raolesong <raolesong@LH900362.dds.com>
2019-07-11 15:33:52 +0800

    eureak-server

--- my-common-parent/my-common-eureka-server/src/main/java/com/song/springcloud/ApplicationTest.java
+++ my-common-parent/my-common-eureka-server/src/main/java/com/song/springcloud/ApplicationTest.java
@@ -2,11 +2,10 @@ package com.song.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer // 声明这是一个Eureka Server
 public class ApplicationTest {
  
 	public static void main(String[] args) {
